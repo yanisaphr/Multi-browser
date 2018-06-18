c3fdf66fb5b96c776893380538a3cde8a48c9430
AGODA\yphrompeng <Yanisa.Phrompeng@agoda.com>
2018-06-18 14:59:37 +0700

    screen shot all page

--- Multi-browser/HelloWorld.cs
+++ Multi-browser/HelloWorld.cs
@@ -17,23 +17,24 @@ namespace Demos
             // Initialize the eyes SDK and set your private API key.
             var eyes = new Eyes();
             eyes.ApiKey = "hTP9KIw4l9LKUGxlQdod46b4vRRvOaF8UfGOs1RmYyo110";
-
+            eyes.ForceFullPageScreenshot = true; 
+ 
             try
             {
                 // Start the test and set the browser's viewport size to 800x600.
                 eyes.Open(driver, "Hello World!", "My first Selenium C# test!", new Size(800, 600));
 
                 // Navigate the browser to the "hello world!" web-site.
-                driver.Url = "https://www.agoda.com";
+                driver.Url = "https://www.agoda.com?expuser=B";
 
                 // Visual checkpoint #1.
                 eyes.CheckWindow("RESORT");
 
                 // Click the "Click me!" button.
-                driver.FindElement(By.XPath("//*[@data-selenium='homesTab']")).Click();
+               // driver.FindElement(By.XPath("//*[@data-selenium='homesTab']")).Click();
 
                 // Visual checkpoint #2.
-                eyes.CheckWindow("RESORT");
+               // eyes.CheckWindow("RESORT");
 
                 // End the test.
                 eyes.Close();
